d948113c23ba79d227b90b867498ebb170bba8b6
Jon Sagara <jonsagara@gmail.com>
2025-05-01 15:43:08 -0700

    Fill the PDF and save to disk.

--- SyncfusionPdfLongText/src/SyncfusionPdfLongText/Helpers/PdfHelper.cs
+++ SyncfusionPdfLongText/src/SyncfusionPdfLongText/Helpers/PdfHelper.cs
@@ -42,7 +42,27 @@ public static class PdfHelper
         DisplayHeader("Filling the two problematic Caliber fields -AND- opening the filled PDF");
         AnsiConsole.WriteLine();
 
-        OpenInDefaultViewer(pdfFilePath: pdfTemplateFilePath);
+
+        PdfLoadedDocument? pdfDocument = null;
+
+        using (var pdfTemplateStream = GetTemplateAsMemoryStream(pdfTemplateFilePath))
+        {
+            pdfDocument = new PdfLoadedDocument(file: pdfTemplateStream);
+
+            FillTop2CaliberFields(pdfDocument, longCaliber1: LongText1, longCaliber2: LongText2);
+
+            // Flatten the form fields so that they can no longer be filled.
+            pdfDocument.Form.Flatten = true;
+
+            // Save the filled form field to a file stream.
+            using (var filledFileStream = File.OpenWrite(renderedPdfPath))
+            {
+                pdfDocument.Save(filledFileStream);
+                pdfDocument.Close();
+            }
+        }
+
+        OpenInDefaultViewer(pdfFilePath: renderedPdfPath);
     }
 
 
@@ -69,6 +89,47 @@ public static class PdfHelper
         }
 
         return pdfTemplateMemoryStream;
+    }
+
+    private static void FillTop2CaliberFields(PdfLoadedDocument pdfDocument, string longCaliber1, string longCaliber2)
+    {
+        FillCaliberField(pdfDocument, fieldName: "topmostSubform[0].Page1[0].Q5_CaliberGauge_1", longCaliber: longCaliber1);
+        FillCaliberField(pdfDocument, fieldName: "topmostSubform[0].Page1[0].Q5_CaliberGauge_2", longCaliber: longCaliber2);
+    }
+
+    private static void FillCaliberField(PdfLoadedDocument pdfDocument, string fieldName, string longCaliber)
+    {
+        if (pdfDocument.Form.Fields.TryGetField(fieldName: fieldName, out PdfLoadedField pdfLoadedField1))
+        {
+            var caliberLoadedTextBoxField = (PdfLoadedTextBoxField)pdfLoadedField1;
+
+            // Fill the .Text property first. If the text doesn't fit, tell Syncfusion to auto-resize the text to fit.
+            caliberLoadedTextBoxField.Text = longCaliber;
+
+            if (IsTextClipped(longCaliber, caliberLoadedTextBoxField))
+            {
+                caliberLoadedTextBoxField.AutoResizeText = true;
+            }
+        }
+    }
+
+    /// <summary>
+    /// Check to see if the text will fit in the text box. If not, make the text box an auto-size field.
+    /// </summary>
+    private static bool IsTextClipped(string? text, PdfLoadedTextBoxField textField)
+    {
+        if (string.IsNullOrWhiteSpace(text))
+        {
+            return false;
+        }
+
+        // The "layouter" determines whether the text will be clipped in the given textbox.
+        var layouter = new PdfStringLayouter();
+        var layoutResult = layouter.Layout(text, textField.Font, new PdfStringFormat(textField.TextAlignment), new SizeF(textField.Bounds.Width, textField.Bounds.Height));
+
+        // If Remainder is a non-null, non-white space string, then there is clipped text, and
+        //   we need to make the field auto-sized.
+        return !string.IsNullOrWhiteSpace(layoutResult.Remainder);
     }
 
     private static void DescribeCaliberTextBoxFields(PdfLoadedDocument pdfDocument)
